9bd559cde1d00ff95860c75510c3a4f801e18e42
SorinJunior <juninhosorin@gmail.com>
2023-01-15 18:01:46 -0300

    9º - commit

--- script.js
+++ script.js
@@ -52,3 +52,22 @@ function bolos(tipoDeBolo) {
     }
     document.getElementById(tipoDeBolo).style.display = "flex";
   }
+
+  //Script para função com scroll suave
+  $(document).ready(function(){
+    $("a").on('click', function(event) {
+  
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1200, function(){
+     
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
