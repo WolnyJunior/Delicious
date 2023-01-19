0576dfdb40d363accd1799da552356aa35f4b748
SorinJunior <juninhosorin@gmail.com>
2023-01-19 20:49:19 -0300

    menu-smartphone

--- script.js
+++ script.js
@@ -85,10 +85,11 @@ function bolos(tipoDeBolo) {
     });
   });
 
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
   }
